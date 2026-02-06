274d3e03e5a5d69e0e7cfb07f3530fcdb86a0ae2
Erick Augusto <contato@eams.dev.br>
2026-02-06 16:01:33 -0300

    Script for getting the link for headings

---
+++ web/scripts/headings.js
@@ -0,0 +1,24 @@
+// Creates a link for each heading across the whole page
+
+document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((heading) => {
+  // Ignores the heading which contains the title of the page
+  if (heading.id != "content-title") {
+
+    // On enter, create and display the link on the right of the heading
+    heading.addEventListener("mouseenter", () => {
+      let link = document.createElement("a")
+
+      link.innerHTML = " #"
+      link.setAttribute("href", `#${heading.id}`)
+      link.setAttribute("title", `${document.getElementById("content-title").innerHTML}#${heading.id}`)
+
+      heading.appendChild(link)
+    })
+
+    // Remove the link once the heading is not on focus
+    heading.addEventListener("mouseleave", () => {
+      heading.removeChild(heading.lastChild)
+    })
+  }
+})
+
